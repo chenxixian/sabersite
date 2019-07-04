bc0ee6810f182f0ab21692d6c1806aa753af51e1
chenxixian <chenxixian@gmail.com>
2019-07-05 00:06:51 +0800

    www

--- paper/saber-browser.js
+++ paper/saber-browser.js
@@ -1,29 +1,29 @@
-import 'modern-normalize/modern-normalize.css';
-import 'typo.css';
-import 'prismjs/themes/prism.css';
-import './styles/global.scss';
+import "modern-normalize/modern-normalize.css";
+//import 'typo.css';
+import "prismjs/themes/prism.css";
+import "./styles/global.scss";
 
 export default ({ router }) => {
   // Progress bar is not needed on server-side
   if (process.browser) {
     // These dependencies are only bundled in client build
-    const nprogress = require('nprogress')
-    require('nprogress/nprogress.css')
+    const nprogress = require("nprogress");
+    require("nprogress/nprogress.css");
 
-    const loaded = Object.create(null)
+    const loaded = Object.create(null);
 
     router.beforeEach((to, from, next) => {
       if (!loaded[to.path]) {
         // Start progress bar before entering page
-        nprogress.start()
+        nprogress.start();
       }
-      next()
-    })
+      next();
+    });
 
     router.afterEach(to => {
-      loaded[to.path] = true
+      loaded[to.path] = true;
       // Stop progress bar after entering page
-      nprogress.done()
-    })
+      nprogress.done();
+    });
   }
-}
+};
