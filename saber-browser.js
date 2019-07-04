415e824bdf1da9181e5ef39fb2b3e2dcc4c224c3
chenxixian <chenxixian@gmail.com>
2019-07-04 23:10:26 +0800

    3333

--- saber-browser.js
+++ saber-browser.js
@@ -1,16 +1,30 @@
-import 'prismjs/themes/prism-tomorrow.css'
+import "prismjs/themes/prism-tomorrow.css";
 // Styles for line highlighting in code blocks
-import 'saber-highlight-css/default.css'
+import "saber-highlight-css/default.css";
 // @ points to current working directory
-import '@/css/global.css'
+import "@/css/global.css";
 
-export default ({ setHead }) => {
-  setHead({
-    link: [
-      {
-        href: 'https://fonts.googleapis.com/css?family=Roboto',
-        rel: 'stylesheet'
+export default ({ router }) => {
+  // Progress bar is not needed on server-side
+  if (process.browser) {
+    // These dependencies are only bundled in client build
+    const nprogress = require("nprogress");
+    require("nprogress/nprogress.css");
+
+    const loaded = Object.create(null);
+
+    router.beforeEach((to, from, next) => {
+      if (!loaded[to.path]) {
+        // Start progress bar before entering page
+        nprogress.start();
       }
-    ]
-  })
-}
+      next();
+    });
+
+    router.afterEach(to => {
+      loaded[to.path] = true;
+      // Stop progress bar after entering page
+      nprogress.done();
+    });
+  }
+};
