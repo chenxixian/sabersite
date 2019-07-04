1cbaa73c2f88136f1193094cb8c1c3ca728f425e
chenxixian <chenxixian@gmail.com>
2019-07-05 01:19:39 +0800

    ddddd

--- paper/saber-node.js
+++ paper/saber-node.js
@@ -1,18 +1,22 @@
-// saber-node.js
 exports.onCreatePages = function() {
-  // Pages are read into the `this.pages` Map
   // Sort posts by createdAt (date) from new to old
   const posts = [...this.pages.values()]
     .filter(page => page.attributes.type === "post" && !page.attributes.draft)
     .sort((a, b) => {
       return a.attributes.createdAt > b.attributes.createdAt ? 1 : -1;
     });
+  const selectFields = page =>
+    page && {
+      title: page.attributes.title,
+      permalink: page.attributes.permalink
+    };
+  this.config.siteConfig.tags = [];
   for (const [index, post] of posts.entries()) {
-    // It's NOT recommended to mutate `page` directly to add addtional props
-    // Use `extendPageProp` instead to add more properties to the `page` prop
-    this.pages.extendPageProp(post.internal.id, {
-      prevPost: this.pages.getPagePublicFields(posts[index - 1]),
-      nextPost: this.pages.getPagePublicFields(posts[index + 1])
-    });
+    post.prevPost = selectFields(posts[index - 1]);
+    post.nextPost = selectFields(posts[index + 1]);
+    this.config.siteConfig.tags = [
+      ...this.config.siteConfig.tags,
+      ...post.attributes.tags
+    ];
   }
 };
